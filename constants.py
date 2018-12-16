803c2847926c2e6e6682e553f60bf447da4b7b40
weida zhong <weida.ustc@gmail.com>
2018-12-16 15:46:28 -0500

    init commit

---
+++ constants.py
@@ -0,0 +1,16 @@
+"""
+Store global constants for maintenance
+
+e.g.
+    filenames
+    column names
+"""
+
+GRAVITY_OF_EARTH = 9.81
+
+ACC_FILE_NAME = 'raw_acc.txt'
+GPS_FILE_NAME = 'gps.txt'
+CALIBRATION_FILE_NAME = 'calibration_para.txt'
+CALI_PARA_FILE = 'cali_para.txt'
+OBD_FILE_NAME = 'raw_obd.txt'
+GYRO_FILE_NAME = 'raw_gyro.txt'
---
+++ utils.py
@@ -0,0 +1,8 @@
+import os
+
+
+def get_default_data_path():
+    """
+    """
+    return os.path.join(os.getcwd(), 'data')
+
