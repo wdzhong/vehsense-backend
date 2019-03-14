5b25ab3befc3e45f6ab48aa897a60402d4fa8121
weida zhong <weida.ustc@gmail.com>
2019-03-14 15:56:43 -0400

    print out the input arguments

--- preprocess.py
+++ preprocess.py
@@ -37,14 +37,20 @@ def preprocess(input_string, configs=None):
             print("ERROR: preprocess(): data path is not set")
             return
 
-        # TODO: accept more flags for clean
         clean_flag = input_map.get('-c', "false")
-        if clean_flag.lower() == 'true':
-            clean_options = ["-d", data_path]
-            clean_file(clean_options)
 
         if debug:
             print("interpolation rate %f Hz" % frequency)
             print("data path: %s" % data_path)
+            print("clean: %s" % clean_flag)
+
+        # TODO: accept more flags for clean
+        if clean_flag.lower() == 'true':
+            clean_options = ["-d", data_path]
+            clean_file(clean_options)
 
         file_process.process_data_main(data_path, frequency, rolling_window_size)
+
+
+if __name__ == "__main__":
+    preprocess(['-d', 'vehsense-backend-data', '-c', 'true'])
